9cad240744ff7f933ef9d8e59445a9e27cef733a
Chase <chasevanhaselen@gmail.com>
2022-04-13 12:08:27 -0500

    dfs

---
+++ dfs.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const dfs = (root) => { // a
+  if (root === null) return [];
+  const res = [];
+  const stack = [root];
+
+  while (stack.length > 0) {
+    let current = stack.pop();
+    res.push(current.val);
+
+    if (current.right) stack.push(current.right); 
+    if (current.left) stack.push(current.left);
+  }
+  return res;
+};
+
+console.log(dfs(a))
